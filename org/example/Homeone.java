3327a05b2d9d07d75e718ffb93ca74b7dd88cfa1
tushar panchani <panchani1008@gmail.com>
2023-02-17 21:54:52 +0000

    homework practice

---
+++ org/example/Homeone.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Homeone {
+    public static void main(String[] args){
+        System.out.println("home one practice");
+        System.out.println("java traning");
+    }
+}
